091cb4068dace4b9cc7555e30394dec75954cbdb
Nikhil Shahi <nshahi1998@gmail.com>
2025-01-08 17:41:59 -0600

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import { HyperbrowserClient } from "./client";
-import {
-  CRAWL_TOOL_OPENAI,
-  CRAWL_TOOL_ANTHROPIC,
-  SCRAPE_TOOL_OPENAI,
-  SCRAPE_TOOL_ANTHROPIC,
-} from "./tools";
 export { HyperbrowserError } from "./client";
-
-export * from "./types";
-export * from "./tools";
 
 // Export HyperbrowserClient as Hyperbrowser for named imports
 export const Hyperbrowser = HyperbrowserClient;
@@ -20,9 +11,4 @@ if (typeof module !== "undefined" && module.exports) {
   module.exports.Hyperbrowser = HyperbrowserClient;
   module.exports.HyperbrowserClient = HyperbrowserClient;
   module.exports.default = HyperbrowserClient;
-
-  module.exports.CRAWL_TOOL_OPENAI = CRAWL_TOOL_OPENAI;
-  module.exports.CRAWL_TOOL_ANTHROPIC = CRAWL_TOOL_ANTHROPIC;
-  module.exports.SCRAPE_TOOL_OPENAI = SCRAPE_TOOL_OPENAI;
-  module.exports.SCRAPE_TOOL_ANTHROPIC = SCRAPE_TOOL_ANTHROPIC;
 }
