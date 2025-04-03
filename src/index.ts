3bed75b4a5ca50f0c1f86c50264645b01e46c536
Nikhil Shahi <nshahi1998@gmail.com>
2025-04-02 22:04:46 -0500

    add Hyperbrowser type

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export { HyperbrowserError } from "./client";
 
 // Export HyperbrowserClient as Hyperbrowser for named imports
 export const Hyperbrowser = HyperbrowserClient;
+// Add a type alias for Hyperbrowser
+export type Hyperbrowser = HyperbrowserClient;
 export default HyperbrowserClient;
 
 // For CommonJS compatibility
