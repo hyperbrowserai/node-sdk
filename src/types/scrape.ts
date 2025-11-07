365abd89f8ec8416ab2b2b96f3428a89d8dc4f1e
Nikhil Shahi <nshahi1998@gmail.com>
2025-11-07 01:51:19 -0600

    change naming

--- src/types/scrape.ts
+++ src/types/scrape.ts
@@ -10,7 +10,7 @@ import { CreateSessionParams } from "./session";
 export interface ScreenshotOptions {
   fullPage?: boolean;
   format?: ScrapeScreenshotFormat;
-  autoCropHeight?: boolean;
+  cropToContent?: boolean;
 }
 
 export interface StorageStateOptions {
